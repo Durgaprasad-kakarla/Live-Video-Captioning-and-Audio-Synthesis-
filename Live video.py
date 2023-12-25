1b84c791fb22236410898ab7e9593776d60a2a82
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-12-25 20:15:04 +0530

    Update and rename Live video 30k.py to Live video.py

--- Live video.py
+++ Live video.py
@@ -1,4 +1,3 @@
-# Create a VideoCapture object to capture video from the default camera
 import base64
 import json
 import collections
@@ -11,7 +10,7 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 import pyttsx3
 app = Flask(__name__)
-model=load_model('model_weights_30k/model_19_30k.h5')
+model=load_model('model_weights_new-500/model_199.h5')
 from keras.applications import ResNet50
 model2=ResNet50(weights="imagenet",input_shape=(224,224,3))
 from keras import Model
@@ -30,9 +29,9 @@ def encode_image(img):
     return feature_vector
 
 
-def get_caption(photo):
+def get_caption(photo,model):
     descriptions = None
-    with open('descriptions__30k.txt', 'r') as f:
+    with open('descriptions_created-500.txt', 'r') as f:
         descriptions = f.read()
     json_string = descriptions.replace("'", "\"")
     descriptions = json.loads(json_string)
@@ -50,12 +49,12 @@ def get_caption(photo):
     for i, word in enumerate(total_words):
         word_to_idx[word] = i + 1
         idx_to_word[i + 1] = word
-    idx_to_word[5119] = "startseq"
-    idx_to_word[5120] = "endseq"
-    word_to_idx["startseq"] = 5119
-    word_to_idx['endseq'] = 5120
+    idx_to_word[177] = "startseq"
+    idx_to_word[178] = "endseq"
+    word_to_idx["startseq"] = 177
+    word_to_idx['endseq'] = 178
     vocab_size = len(word_to_idx) + 1
-    maxlen=74
+    maxlen=19
 
     in_text='startseq'
     for i in range(maxlen):
@@ -76,98 +75,81 @@ import threading
 import time
 
 # Function to perform video processing
-def process_video(video_capture):
-    # Set the camera properties
-    video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Function to capture video frames and process them
+def process_video(video_capture, frames_with_captions, interval, output_folder, model):
+    # Set the camera properties (you can adjust these as needed)
+    video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
+    video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 900)
 
-    # Initialize variables
     frame_count = 0
     start_time = time.time()
 
-    # Example function to get caption based on frame number
     while True:
-        # Read a frame from the video capture
-        ret, frame = video_capture.read()
+        ret, frame = video_capture.read(0)
 
-        # Check if the frame reading was successful
         if not ret:
             print("Failed to read a frame.")
             break
 
-        # Calculate the elapsed time
         elapsed_time = time.time() - start_time
 
-        # Check if the desired interval has passed
         if elapsed_time >= interval:
-            # Save the frame as an image file
             frame_path = os.path.join(output_folder, f"frame_{frame_count}.jpg")
             cv2.imwrite(frame_path, frame)
 
-            # Get the caption for the current frame
             photo_2048 = encode_image(frame_path).reshape((1, 2048))
-            caption = get_caption(photo_2048)
+            caption = get_caption(photo_2048, model)
 
-            # Reset the start time and increment the frame count
             start_time = time.time()
             frame_count += 1
 
-            # Add the caption to the frame
             frames_with_captions.append((frame, caption))
 
-    # Release the video capture object
-    video_capture.release()
 
 # Function to perform audio insertion
-def insert_audio():
+def insert_audio(frames_with_captions):
     engine = pyttsx3.init()
 
     while True:
         if frames_with_captions:
             frame, caption = frames_with_captions.pop(0)
 
-            # Add the caption to the frame
             cv2.putText(frame, caption, (10, frame.shape[0] - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
 
-            # Speak the caption
             engine.say(caption)
             engine.runAndWait()
 
-            # Display the frame
             cv2.imshow("Live Video", frame)
 
-        # Check for keyboard interrupt (press 'q' to exit)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Close any open windows
     cv2.destroyAllWindows()
 
-# Main program
+4
 if __name__ == "__main__":
-    # Create a folder to store the extracted frames
     output_folder = "frames_output"
     os.makedirs(output_folder, exist_ok=True)
-
-    # Initialize variables
     frames_with_captions = []
-    interval = 10
+    interval = 6 # Adjust the interval as needed for your desired frame rate
 
-    # Create a video capture object
     video_capture = cv2.VideoCapture(0)
 
-    # Check if the video capture is successfully opened
     if not video_capture.isOpened():
         print("Failed to open video capture.")
         exit()
 
+    # Load your captioning model here (replace 'your_model.h5' with your actual model path)
+    model = load_model('model_weights_new-500/model_199.h5')
+
     # Create and start the video processing thread
-    video_thread = threading.Thread(target=process_video, args=(video_capture,))
+    video_thread = threading.Thread(target=process_video,
+                                    args=(video_capture, frames_with_captions, interval, output_folder, model))
     video_thread.start()
 
     # Start the audio insertion function in the main thread
-    insert_audio()
+    insert_audio(frames_with_captions)
 
     # Wait for the video processing thread to finish
     video_thread.join()
